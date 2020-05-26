b3cfdeca81ab5ed426c769b86e55d3cec5c32233
t-tajiri <swallow.ava@gmail.com>
2020-05-26 20:00:59 +0900

    feat: create producer

--- pages/mediasoup.ts
+++ pages/mediasoup.ts
@@ -1,8 +1,13 @@
-const mediasoupClient = require('mediasoup-client')
+import { Device } from 'mediasoup-client'
+
 const io = require('socket.io-client')
 const socketPromise = require('socket.io-promise')
 
+//TODO change to import statement from require
+//TODO define types
+//TODO remove global variables
 let device
+let producer
 
 export async function connect() {
   const urlParams = new URLSearchParams(window.location.search)
@@ -25,11 +30,76 @@ export async function connect() {
     const routerRtpCapabilities = await socket.request('getRouterRtpCapabilities')
 
     try {
-      device = new mediasoupClient.Device();
+      device = new Device();
     } catch (err) {
       console.error(err);
     }
 
     await device.load({ routerRtpCapabilities });
   })
+
+  const data = await socket.request('createProducerTransport', {
+    rtpCapabilities: device.rtpCapabilities
+  })
+
+  const transport = device.createSendTransport();
+
+  transport.on('connect', async ({ dtlsParameters }, cb, errback) => {
+    try {
+      socket.request('transport-connect', {
+        transportId: transport.id,
+        dtlsParameters
+      })
+
+      cb()
+    } catch (errorContent) {
+      errback(errorContent)
+    }
+  })
+
+  transport.on('produce', async ({ kind, rtpParameters, appData }, cb, errback) => {
+    try {
+      const { id } = await socket.request('transport-produce', {
+        transportId: transport.id,
+        kind,
+        rtpParameters,
+        appData
+      })
+
+      cb({ id })
+    } catch (errorContent) {
+      errback(errorContent)
+    }
+  })
+
+  let stream
+  try {
+    stream = await getUserMedia(transport)
+    const track = stream.getVideoTracks()[0]
+
+    producer = await transport.produce({
+      track,
+      encodings: [
+        { maxBitrate: 350000 },
+        { maxBitrate: 650000 }
+      ]
+    })
+  }
+}
+
+async function getUserMedia(transport) {
+  if (!device.canProduce('video')) {
+    console.error('cannot produce video')
+    return
+  }
+
+  let stream
+  try {
+    stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true })
+  } catch (err) {
+    console.error('getUserMedia() failed:', err.message)
+    throw err
+  }
+
+  return stream
 }
